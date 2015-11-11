5bf7838722d6d3b9bfc1dec90dd140e77065c2ca
JRMeyer <joshua.richard.meyer@gmail.com>
2015-11-11 12:27:40 -0700

    made the phonetic dictionary maker work for kazakh or kyrgyz, cleaned up a little

--- create_phonetic_dict.py
+++ create_phonetic_dict.py
@@ -1,96 +1,145 @@
 from tokenize_corpus import tokenize_line
 import re
 
-def save_pronunciation_dict(tokens, lookupTable):
-    outFile = open('kyrgyz.dict', mode='wt', encoding='utf-8') 
+def save_pronunciation_dict(tokens, lookupTable, lang):
+    outFile = open('language.dict', mode='wt', encoding='utf-8') 
 
     for token in sorted(set(tokens)):
         phonemes=token
 
-        # palatal/uvular plosives followed by front/back vowels
-        phonemes = re.sub(r'к([аоуы])', r'kh \1', phonemes)
-        phonemes = re.sub(r'к([иеэөү])', r'k \1', phonemes)
-        phonemes = re.sub(r'г([аоуы])', r'gh \1', phonemes)
-        phonemes = re.sub(r'г([иеэөү])', r'g \1', phonemes)
+        ###
+        ##
+        #
+        
+        if lang == 'kyrgyz':
+            # plosives followed by front/back vowels
+            phonemes = re.sub(r'к([аоуы])', r'kh \1', phonemes)
+            phonemes = re.sub(r'к([иеэөү])', r'k \1', phonemes)
+            phonemes = re.sub(r'г([аоуы])', r'gh \1', phonemes)
+            phonemes = re.sub(r'г([иеэөү])', r'g \1', phonemes)
 
-        # syllable final palatal/velar plosives preceded by front/back vowels
-        phonemes = re.sub(r'([аоуы])к([^аоуыиеэөү])', r'\1kh \2', phonemes)
-        phonemes = re.sub(r'([иеэөү])к([^аоуыиеэөү])', r'\1k \2', phonemes)
-        phonemes = re.sub(r'([аоуы])г([^аоуыиеэөү])', r'\1gh \2', phonemes)
-        phonemes = re.sub(r'([иеэөү])г([^аоуыиеэөү])', r'\1g \2', phonemes)
+            # syllable final plosives preceded by front/back vowels
+            phonemes = re.sub(r'([аоуы])к([^аоуыиеэөү])', r'\1kh \2', phonemes)
+            phonemes = re.sub(r'([иеэөү])к([^аоуыиеэөү])', r'\1k \2', phonemes)
+            phonemes = re.sub(r'([аоуы])г([^аоуыиеэөү])', r'\1gh \2', phonemes)
+            phonemes = re.sub(r'([иеэөү])г([^аоуыиеэөү])', r'\1g \2', phonemes)
 
-        # word final palatal/velar plosives preceded by front/back vowels
-        phonemes = re.sub(r'([аоуы])к($)', r'\1kh', phonemes)
-        phonemes = re.sub(r'([иеэөү])к($)', r'\1k', phonemes)
-        phonemes = re.sub(r'([аоуы])г($)', r'\1gh', phonemes)
-        phonemes = re.sub(r'([иеэөү])г($)', r'\1g', phonemes)
+            # word final plosives preceded by front/back vowels
+            phonemes = re.sub(r'([аоуы])к($)', r'\1kh', phonemes)
+            phonemes = re.sub(r'([иеэөү])к($)', r'\1k', phonemes)
+            phonemes = re.sub(r'([аоуы])г($)', r'\1gh', phonemes)
+            phonemes = re.sub(r'([иеэөү])г($)', r'\1g', phonemes)
 
-        # /b/ between back vowels goes to [w] 
-        phonemes = re.sub(r'([аоуы])б([аоуы])', r'\1b \2', phonemes)
+            # /b/ between back vowels goes to [w] 
+            phonemes = re.sub(r'([аоуы])б([аоуы])', r'\1b \2', phonemes)
 
-        # diphthongs
-        phonemes = re.sub(r'ой', r'o i ', phonemes)
-        phonemes = re.sub(r'ай', r'a i ', phonemes)
+            # diphthongs
+            phonemes = re.sub(r'ой', r'o i ', phonemes)
+            phonemes = re.sub(r'ай', r'a i ', phonemes)
 
+        elif lang == 'kazakh':
+            pass
+
+        #
+        ##
+        ###
+        
         for character in phonemes:
             if character in lookupTable:
                 phonemes = re.sub(character, lookupTable[character], phonemes)
 
         # in case we added a space to the end of the sequence
-        phonemes=phonemes.strip()
+        phonemes = phonemes.strip()
         
-        print((token + ' ' + phonemes), end="\n", file=outFile)
+        print((token + ' ' + phonemes), end='\n', file=outFile)
 
 
-# based on Arpabet https://en.wikipedia.org/wiki/Arpabet
-lookupTable = {'а':'a ',
+kyrgyz_table = {'а':'a ', # back vowels
                'о':'o ',
                'у':'u ',
-               'ы':'y ',
-            
-               'и':'i ',
+               'ы':'ih ',
+               'и':'i ', # front vowels
                'е':'e ',
                'э':'e ',
-               'ө':'o ',
-               'ү':'u ',
-               
-               'ю':'u ',
-               'я':'a ',
-               'ё':'o ',
-               
-               'п':'p ',
+               'ө':'oe ',
+               'ү':'y ',
+               'ю':'j u ', # glide vowels
+               'я':'j a ',
+               'ё':'j o ',
+               'п':'p ', # bilabials
                'б':'b ',
-               
-               'д':'d ',
+               'д':'d ', # coronals
                'т':'t ',
-
-               'к':'k ',
+               'к':'k ', # velars
                'г':'g ',
-               
-               'ш':'sh ',
-               'щ':'sch ',
+               'х':'h ',
+               'ш':'sh ', # (alveo)(palatals)
+               'щ':'sh ',
                'ж':'zh ',
-            
-               'й':'j ',
+               'з':'z ', 
+               'с':'s ',
+               'ц':'ts ', # affricates
+               'ч':'ch ',
+               'й':'j ', # glides
                'л':'l ',
-               'м':'m ',
+               'м':'m ', # nasals
                'н':'n ',
                'ң':'ng ',
-               
-               'з':'z ',
-               'с':'s ',
-               'ц':'c ',
-               'ч':'ch ',
-               'ф':'f ',
+               'ф':'f ', # labiodentals
                'в':'v ',
-               'х':'h ',
-               'р':'r ',
+               'р':'r ', # trill
                'ъ':' ',
                'ь':' '}
         
+
+
+kazakh_table = {'б' : 'b ',
+                'в' : 'v ',
+                'г' : 'g ',
+                'д' : 'd ',
+                'з' : 'z ',
+                'к' : 'k ',
+                'л' : 'l ',
+                'м' : 'm ',
+                'н' : 'n ',
+                'п' : 'p ',
+                'р' : 'r ',
+                'с' : 's ',
+                'т' : 't ',
+                'ф' : 'f ',
+                'х' : 'h ',
+                'һ' : 'h ', 
+                'ж' : 'zh ',
+                'ц' : 'ts ',
+                'ч' : 'ch ',
+                'ш' : 'sh ',
+                'щ' : 'sh ',
+                'й' : 'j ',
+                'ғ' : 'gh ',
+                'қ' : 'kh ',
+                'ң' : 'ng ',
+                'а' : 'a ', # vowels
+                'ә' : 'a ',
+                'у' : 'u ',
+                'ү' : 'y ',
+                'ұ' : 'o ',
+                'о' : 'o ',
+                'ө' : 'oe ',
+                'э' : 'e ',
+                'е' : 'e ',
+                'и' : 'i ',
+                'і' : 'ih ',
+                'ы' : 'ih ',
+                'я' : 'j a ', # glide + vowels
+                'ё' : 'j o ',
+                'ю' : 'j u ',
+                'ь' : '',
+                'ъ' : ''}
+
+
         
-if __name__ == "__main__":
-    fileName = input("enter file path here: ")
+if __name__ == '__main__':
+    fileName = input('enter file path here: ')
     f = open(fileName)
 
     # regex pattern to match everything that isn't a letter
@@ -98,7 +147,8 @@ if __name__ == "__main__":
     
     tokens=[]
     for line in f:
-        for token in tokenize_line(line, n=1, tags=False):
-            token = pattern.sub('', token)
-            tokens.append(token)
-    save_pronunciation_dict(tokens, lookupTable)
+        tokens.append(line.split(' ')[0])
+        # for token in tokenize_line(line, n=1, tags=False):
+        #     token = pattern.sub('', token)
+        #     tokens.append(token)
+    save_pronunciation_dict(tokens, lookupTable=kyrgyz_table, lang='kyrgyz')
--- nltk_tagging.py
+++ nltk_tagging.py
@@ -1,15 +1,16 @@
 import nltk
 from nltk import corpus
 from nltk.tag import DefaultTagger, RegexpTagger, NgramTagger, hmm
+import matplotlib.pyplot as plt
 
 
-def get_train_test_sets(percentTrain):
+def get_train_test_sets(percentTrain, percentTest):
     sents=[]
     for sent in corpus.treebank.tagged_sents():
         sents.append(sent)
 
     trainSents = sents[:int(round((len(sents)*percentTrain),0))]
-    testSents = sents[int(round((len(sents)*percentTrain),0)):]
+    testSents = sents[int(round((len(sents)*(1-percentTest)),0)):]
     return trainSents,testSents
 
 
@@ -33,6 +34,7 @@ def train_taggers(trainSents):
 
     Taggers = [default_tagger,unigram_tagger,bigram_tagger,
                trigram_tagger, regexp_tagger, hmm_tagger]
+
     return Taggers
 
 
@@ -58,9 +60,44 @@ def test_tagger(Tagger,testSents):
     print(1-sentenceError/sentenceTotal)
     print(1-wordError/wordTotal)
 
+    
+def plot_nltk_hmm_performance():
+    performances = [(1, 55.4070597837176),
+                    (2, 63.85431544582738),
+                    (3, 68.27178126912875),
+                    (4, 70.47541318098347),
+                    (5, 71.92409712303611),
+                    (10, 76.60681493572741),
+                    (25, 82.62599469496021),
+                    (50, 86.47214854111406),
+                    (75, 88.87982044480718),
+                    (90, 89.92042440318302)]
 
+    X=[]
+    Y=[]
+    for x,y in performances:
+        X.append(x)
+        Y.append(y)
+
+
+    plt.plot(X,Y)
+    plt.xlabel("Percent of Penn Treebank used at Train")
+    plt.ylabel("Percent Word Accuracy at Test")
+    plt.title("Performance of NLTK HMM POS Tagger")
+    plt.show()
+
+
+    
 if __name__ == "__main__":
-    trainSents, testSents = get_train_test_sets(percentTrain=.9)
-    Taggers = train_taggers(trainSents)
-    for Tagger in Taggers:
-        test_tagger(Tagger, testSents)
+    # training=[.01, .02, .03, .04, .05, .10, .25, .50, .75, .90]
+    training=[.90]
+    plot_nltk_hmm_performance()
+    for percentTrain in training:
+        trainSents, testSents = get_train_test_sets(percentTrain,percentTest=.1)
+        print(len(trainSents))
+        print(len(testSents))
+        Taggers = train_taggers(trainSents)
+        for Tagger in Taggers:
+            test_tagger(Tagger, testSents)
+
+
